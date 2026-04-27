5430474ecb694e13526b37c7c0bc8a55b697db32
ghaidaabenrekia-dot <ghaidaa.benrekia@codedbykids.org>
2026-04-27 22:55:45 +0000

    learned about else and else if

---
+++ app.js
@@ -0,0 +1,33 @@
+// let headsOrTails="Tails"
+// // if (headsOrTails==="heads"){
+// //     console.log("jackson wins 100 bucks")
+// // }
+
+// // if (headsOrTails==="Tails"){
+// //     console.log("i win 499 bucks, and emilio wins 1 buck")
+// // }
+
+// if (headsOrTails==="heads"){
+//     console.log("jackson wins 100 bucks")
+// } else {
+//     console.log("i win 499 bucks, emilio wins one buck")
+// }
+
+let binary=1
+if (binary===1){
+    console.log("binary")
+}
+else {
+    console.log("non-binary")
+}
+let color="yellow"
+if (color==="yellow"){
+    console.log("slow-down")
+}
+else if(color==="red"){
+    console.log("stop")
+}
+else {
+    console.log("go")
+}
+    
